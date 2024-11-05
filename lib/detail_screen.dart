8435f83ea60572994c63268d3b458ec48d520645
Farrel44 <farrellasso69@gmail.com>
2024-11-05 13:23:33 +0700

    revise main screen

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,144 +1,98 @@
 import 'package:flutter/material.dart';
+import 'package:aplikasi_wisata/model/tourism_place.dart';
 
 class DetailScreen extends StatelessWidget{
-  const DetailScreen({Key? key}) : super(key: key);
-
+  final TourismPlace place;
+  const DetailScreen({Key? key, required this.place}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     var informationTextStyle = const TextStyle(fontFamily: 'Oxygen');
     return Scaffold(
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Image.asset('images/baturraden1.jpg'),
-              Container(
-                margin: const EdgeInsets.only(top: 16.0),
-                child: const Text(
-                  'Baturraden',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 30.0,
-                    fontFamily: 'Staatliches',
-                  ),
-                  )
+      backgroundColor: Colors.black,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Image.asset(place.imageAsset),
+            Container(
+              margin: const EdgeInsets.only(top: 16.0),
+              child: Text(
+                place.name,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontFamily: 'Staatliches',
                 ),
-                Container(
-                  margin: const EdgeInsets.symmetric(vertical: 16.0,),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
                     children: <Widget>[
-                      Column(
-                        children: <Widget>[
-                          const Icon(Icons.calendar_today),
-                          const SizedBox(height: 8.0,),
-                          Text('Buka setiap hari',
-                          style: informationTextStyle,)
-                        ],
+                      const Icon(Icons.calendar_today),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        place.openDays,
+                        style: informationTextStyle,
                       ),
-                      Column(
-                        children: <Widget>[
-                          const Icon(Icons.lock_clock),
-                          const SizedBox(height: 8.0),
-                          Text("07.00 - 17.00",
-                          style: informationTextStyle,)
-                        ],
-                      ),
-                      Column(
-                        children: <Widget>[
-                          const Icon(Icons.monetization_on),
-                          const SizedBox(height: 8.0),
-                          Text("RP 25.000",
-                          style: informationTextStyle,)
-                        ],
-                      )
                     ],
                   ),
-                ),
-                Container(
-                  padding: const EdgeInsets.all(16.0),
-                  child: const Text(
-                    "Taman hutan alami yang tertata rapi, sejuk, nyaman , bersih dan aman. Pedagang makanan khas seperti sate kelinci, tempe mendoan, nasi pecel, berjajar rapi di tepi jalan dengan harga tetap bersahabat.",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 16.0),
+                  Column(
+                    children: <Widget>[
+                      const Icon(Icons.access_time),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        place.openTime,
+                        style: informationTextStyle,
+                      ),
+                    ],
                   ),
-                ),
-                SizedBox(
-                  height: 159,
-                  child: ListView(
-                    
-                    scrollDirection: Axis.horizontal,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(8.0),
-                          child: Image.network(
-                            'https://atourin.obs.ap-southeast-3.myhuaweicloud.com/images/destination/banyumas/baturraden-adventure-forest-profile1672684727.jpeg?x-image-process=image/resize,p_100,limit_1/imageslim'
-                          ),
-                        )
+                  Column(
+                    children: <Widget>[
+                      const Icon(Icons.monetization_on),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        place.ticketPrice,
+                        style: informationTextStyle,
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child:ClipRRect(
-                          borderRadius: BorderRadius.circular(8),
-                          child: Image.network(
-                            'https://yukdolan.com/wp-content/uploads/2023/02/Bukit-Bintang-Baturraden.jpg.webp'
-                        ),
-                        ) 
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child:ClipRRect(
-                          borderRadius: BorderRadius.circular(8),
-                          child: Image.network(
-                            'https://o-cdn-cas.oramiland.com/parenting/images/Curug_Telu_Wisata_Baturaden_insta.width-800.format-webp.webp'
-                        ),
-                        ) 
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child:ClipRRect(
-                          borderRadius: BorderRadius.circular(8),
-                          child: Image.network(
-                            'https://o-cdn-cas.oramiland.com/parenting/images/Curug_Bidadari_Baturaden.width-800.format-webp.webp'
-                        ),
-                        ) 
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child:ClipRRect(
-                          borderRadius: BorderRadius.circular(8),
-                          child: Image.network(
-                            'https://o-cdn-cas.oramiland.com/parenting/images/Curug_Jenggala.width-800.format-webp.webp'
-                        ),
-                        ) 
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child:ClipRRect(
-                          borderRadius: BorderRadius.circular(8),
-                          child: Image.network(
-                            'https://o-cdn-cas.oramiland.com/parenting/images/Telaga_Sunyi.width-800.format-webp.webp'
-                        ),
-                        ) 
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child:ClipRRect(
-                          borderRadius: BorderRadius.circular(8),
-                          child: Image.network(
-                            'https://o-cdn-cas.oramiland.com/parenting/images/wisata-baturaden.width-800.format-webp.webp'
-                        ),
-                        ) 
-                      ),
-                    ]
+                    ],
                   ),
-                )
-            ],
-          ),
-        )
-      );
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                place.description,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 16.0,
+                  fontFamily: 'Oxygen',
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: place.imageUrls.map((url) {
+                  return Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(url),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:aplikasi_wisata/detail_screen.dart';
-
+import 'package:aplikasi_wisata/model/tourism_place.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -11,44 +11,48 @@ class MainScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Wisata Purwokerto'),
       ),
-      body: InkWell(
-        onTap: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) {
-            return const DetailScreen();
-          }));
-        },
-        child: Card(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                flex: 2,
-                child: Image.asset('images/baturraden3.jpg'),
-              ),
-              const Expanded(
-                flex: 2,
-                child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Text(
-                        'Baturraden',
-                        style: TextStyle(fontSize: 16.0),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Text('Baturaden menjadi destinasi wisata yang wajib dikunjungi jika berkunjung ke wilayah Banyumas.')
-                    ],
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final TourismPlace place = tourismPlaceList[index];
+          return InkWell(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return DetailScreen(place: place,);
+              }));
+            },
+            child: Card(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget> [
+                  Expanded(
+                    flex: 2,
+                    child: Image.asset(place.imageAsset),
                   ),
-                )
-              )
-            ],
-          ),
-        ),
-
+                  Expanded(
+                    flex: 2,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            place.name,
+                            style: const TextStyle(fontSize: 16.0),
+                          ),
+                          const SizedBox(
+                            height: 19,
+                          ),
+                          Text(place.location)
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+        itemCount: tourismPlaceList.length,
       )
     );
   }
